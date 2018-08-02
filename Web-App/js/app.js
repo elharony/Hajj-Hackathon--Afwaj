8409565589933fd43ec576c02bb5fcdb319970ea
Yahya Elharony <yahyaelharony@gmail.com>
2018-08-02 21:02:27 +0300

    Dynamic 'Trips' data instead of the old static one

--- Web-App/js/app.js
+++ Web-App/js/app.js
@@ -103,3 +103,156 @@ initMap = () => {
         })
     })
 }
+
+
+
+
+
+
+
+/******* Trips *******/
+
+// Trips [ Data ]
+const trips = [
+    {
+        trip: { 
+            id: 1,
+            from: 'Makkah',
+            to: 'Muzdalifah',
+            date: '2-8-2018',
+        },
+        group: {
+            name: 'EGY-439',
+            pilgrims: 200,
+            statusCondition: 'success',
+            statusText: 'Ongoing',
+            liveData: {
+                safe: 200,
+                warning: 0,
+                danger: 0
+            }
+        }
+    },
+    {
+        trip: { 
+            id: 2,
+            from: 'Madinah',
+            to: 'Arafat',
+            date: '3-8-2018',
+        },
+        group: {
+            name: 'PAK-129',
+            pilgrims: 120,
+            statusCondition: 'danger',
+            statusText: 'Stopped',
+            liveData: {
+                safe: 118,
+                warning: 0,
+                danger: 2
+            }
+        }
+    },
+    {
+        trip: { 
+            id: 3,
+            from: 'Safa W Marwa',
+            to: 'Madinah',
+            date: '4-8-2018',
+        },
+        group: {
+            name: 'IND-20',
+            pilgrims: 40,
+            statusCondition: 'warning',
+            statusText: 'Pending',
+            liveData: {
+                safe: 0,
+                warning: 0,
+                danger: 0
+            }
+        }
+    },
+    {
+        trip: { 
+            id: 4,
+            from: 'Madinah',
+            to: 'Makkah',
+            date: '6-8-2018',
+        },
+        group: {
+            name: 'EGY-98',
+            pilgrims: 55,
+            statusCondition: 'success',
+            statusText: 'Ongoing',
+            liveData: {
+                safe: 54,
+                warning: 1,
+                danger: 0
+            }
+        }
+    }
+]
+
+// Trips [ Update the UI ]
+const tripsTable = document.querySelector(".trips-table")
+
+trips.map((thisTrip) => {
+    let row = document.createElement("tr")
+    row.innerHTML = `
+                        <td>${thisTrip.trip.id}</td>
+                        <td>${thisTrip.trip.from}</td>
+                        <td>${thisTrip.trip.to}</td>
+                        <td>${thisTrip.trip.date}</td>
+                        <td>${thisTrip.group.name}</td>
+                        <td>${thisTrip.group.pilgrims}</td>
+                        <td><span class="notification notification-${thisTrip.group.statusCondition}">${thisTrip.group.statusText}</span></td>
+                        <td>
+                            <span class="${thisTrip.group.liveData.safe > 0 ? 'notification notification-success' : 'notification'}">${thisTrip.group.liveData.safe}</span>
+                            <span class="${thisTrip.group.liveData.warning > 0 ? 'notification notification-warning' : 'notification'}">${thisTrip.group.liveData.warning}</span>
+                            <span class="${thisTrip.group.liveData.danger > 0 ? 'notification notification-danger' : 'notification'}">${thisTrip.group.liveData.danger}</span>
+                        </td>
+                        <td>
+                            <button type="button" class="option btn btn-default" data-toggle="modal" data-target="#modal-${thisTrip.trip.id}">
+                                <i class="material-icons">create</i>
+                            </button>
+                        </td>
+                        <div class="modal fade" id="modal-${thisTrip.trip.id}" tabindex="-1" role="dialog" aria-labelledby="modal-${thisTrip.trip.id}" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="modal-${thisTrip.trip.id}">Assign A Trip Guide</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label class="bmd-label-floating">Hajj Group</label>
+                                                <input type="text" class="form-control" value="Egypt Group" disabled>
+                                            </div>
+                                            </div>
+                                            <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label class="bmd-label-floating">Group Guide</label>
+                                                <select class="form-control">
+                                                    <option value="Mohamed Mously">Mohamed Mously</option>
+                                                    <option value="Yahya Elharony">Yahya Elharony</option>
+                                                </select>
+                                            </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-success" data-dismiss="modal">Save changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    `
+
+    tripsTable.appendChild(row)
+})
